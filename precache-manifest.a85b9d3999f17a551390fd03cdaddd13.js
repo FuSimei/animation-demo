f01bb2b673458690da01adad6c0854da4fabd372
FuSimei <FuSimei@users.noreply.github.com>
2020-05-15 12:45:21 +0000

    Automated deployment: Fri May 15 12:45:21 UTC 2020 af22a91fabcad35e40809788004a697a9190c221

---
+++ precache-manifest.a85b9d3999f17a551390fd03cdaddd13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765fff6076593c670dffcbd5bfe024e1",
+    "url": "/animation-demo/index.html"
+  },
+  {
+    "revision": "a2c9a8eff166feda54ac",
+    "url": "/animation-demo/static/css/main.59f7c045.chunk.css"
+  },
+  {
+    "revision": "205d87e5dc9d41657f97",
+    "url": "/animation-demo/static/js/2.9b8303b0.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/animation-demo/static/js/2.9b8303b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c9a8eff166feda54ac",
+    "url": "/animation-demo/static/js/main.8387a911.chunk.js"
+  },
+  {
+    "revision": "3b749177b7279af71ad2",
+    "url": "/animation-demo/static/js/runtime-main.93ec7a07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a61a39466e6ab25549ac1e5848c4c959.js"
+  "/animation-demo/precache-manifest.a85b9d3999f17a551390fd03cdaddd13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/animation-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
